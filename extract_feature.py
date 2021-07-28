6d01848f08a0b4e8e132de3dcfd1d6b053ac3057
bearhsiang <b05902066@ntu.edu.tw>
2021-07-29 01:33:55 +0800

    new extract feature with torch Dataset and DataLoader

--- extract_feature.py
+++ extract_feature.py
@@ -4,7 +4,60 @@ import argparse
 from tqdm.auto import tqdm
 import numpy as np
 from pathlib import Path
+from torch.utils.data import Dataset, DataLoader
 import csv
+
+class TSVDataset(Dataset):
+
+    def __init__(self, tsv, path_key, audio_dir, target_sample_rate=16000):
+
+        self.data = []
+        self.path_key = path_key
+        self.audio_dir = Path(audio_dir)
+        self.target_sample_rate = target_sample_rate
+
+        with open(tsv, 'r') as f:
+            reader = csv.DictReader(
+                f,
+                delimiter='\t',
+                quotechar=None,
+                doublequote=False,
+                lineterminator='\n',
+                quoting=csv.QUOTE_NONE,
+            )
+            for line in reader:
+                self.data.append(line)
+
+        self.resamplers = {}
+
+    def __getitem__(self, idx):
+
+        wav_file = self.audio_dir / self.data[idx][self.path_key]
+        wav, sr = torchaudio.load(wav_file)
+        
+        if sr != self.target_sample_rate:
+            if sr not in self.resamplers:
+                self.resamplers[sr] = torchaudio.transforms.Resample(
+                    orig_freq = sr,
+                    new_freq = self.target_sample_rate,
+                )
+            wav = self.resamplers[sr](wav)
+
+        return {
+            'wav': wav,
+            'data': self.data[idx]
+        }
+
+    def __len__(self):
+        return len(self.data)
+
+    def collate_fn(self, samples):
+
+        return {
+            'wav': [sample['wav'] for sample in samples],
+            'data': [sample['data'] for sample in samples],
+        }
+
 
 if __name__ == '__main__':
 
@@ -20,77 +73,54 @@ if __name__ == '__main__':
     parser.add_argument('-f', '--feature', required=True)
     parser.add_argument('-r', '--sample-rate', default=16000)
     parser.add_argument('-b', '--batch-size', default=1, type=int)
+    parser.add_argument('-n', '--num_workers', default=1, type=int)
     args = parser.parse_args()
 
     device = 'cuda' if args.gpu else 'cpu'
     feature_list = torch.hub.list('s3prl/s3prl')
+
     if args.feature not in feature_list:
         print(f'feature "{args.feature}" is not available. Please choose one of {feature_list}')
         exit(1)
 
-    audio_dir = Path(args.audio_dir)
-    output_dir = Path(args.output_dir)
+    dataset = TSVDataset(
+        args.input_tsv,
+        args.path_key,
+        args.audio_dir,
+        args.sample_rate,
+    )
 
-    output_dir.mkdir(parents=True, exist_ok=True)
+    dataloader = DataLoader(
+        dataset, 
+        batch_size = args.batch_size,
+        num_workers = args.num_workers,
+        collate_fn = dataset.collate_fn,
+    )
 
     extractor = torch.hub.load('s3prl/s3prl', args.feature).to(device)
 
-    lines = []
-    with open(args.input_tsv, 'r') as f:
-        reader = csv.DictReader(
-            f,
-            delimiter='\t',
-            quotechar=None,
-            doublequote=False,
-            lineterminator='\n',
-            quoting=csv.QUOTE_NONE,
-        )
-        for line in reader:
-            lines.append(line)
-
-
-    resamplers = {}
-
     data = []
+    output_dir = Path(args.output_dir)
+    output_dir.mkdir(parents=True, exist_ok=True)
 
-    for offset in tqdm(range(0, len(lines), args.batch_size)):
+    for batch in tqdm(dataloader):
 
-        batch = lines[offset: offset+args.batch_size]
-        src_batch = []
-
-        for line in batch:
-            src, sr = torchaudio.load(audio_dir/line[args.path_key])
-            if sr not in resamplers:
-                resamplers[sr] = torchaudio.transforms.Resample(
-                    orig_freq = sr,
-                    new_freq = args.sample_rate,
-                )
-            src = resamplers[sr](src)
-            src_batch.append(src)
+        batch['wav'] = [wav.to(device) for wav in batch['wav']]
 
         with torch.no_grad():
-            feature_batch = extractor(src_batch)['last_hidden_state']
+            features = extractor(batch['wav'])['last_hidden_state']
 
-        for line, feature in zip(batch, feature_batch):
-
-            index = line[args.path_key].rsplit('.', maxsplit=1)[0]
-            np.save(output_dir/index, feature)
-
-            item = {
-                'id': index,
-                'path': f'{index}.npy',
-                'n_frames': feature.size(0),
-                'tgt_text': line[args.tgt_key]
-            }
-
-            if args.src_key != None:
-                item['src_text'] = line[args.src_key]
-
+        ratio = max([len(wav) for wav in batch['wav']]) / features.size(1)
+        features = [feature[:round(len(feature)*ratio)].cpu().numpy() for feature in features]
+    
+        for feature, item in zip(features, batch['data']):
+            
+            ID = item[args.path_key].rsplit('.', maxsplit=1)[0]
+            np.save(output_dir/f'{ID}.npy', feature)
+            item['feature'] = f'{ID}.npy'
             data.append(item)
 
-    header = ['id', 'path', 'n_frames', 'tgt_text']
-    if args.src_key != None:
-        header.append('src_text')
+    header = data[0].keys()
 
     with open(args.output_tsv, 'w') as f:
         writer = csv.DictWriter(
@@ -100,8 +130,7 @@ if __name__ == '__main__':
             doublequote=False,
             lineterminator='\n',
             quoting=csv.QUOTE_NONE,
-            fieldnames=header
+            fieldnames=header,
         )
         writer.writeheader()
         writer.writerows(data)
-
