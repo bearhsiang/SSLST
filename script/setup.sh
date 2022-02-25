9446bf2425df7128964d50025c153673bbf11a92
bearhsiang <b05902066@ntu.edu.tw>
2022-02-25 22:40:19 +0800

    add setup.sh

---
+++ script/setup.sh
@@ -0,0 +1,6 @@
+#!/usr/local/bin bash
+
+export sslst_data_root=~/SSLST/data
+export sslst_feat_root=/ssd/ssl_feat
+export sslst_output_root=/hdd/ssl_results
+export sslst_data_bin_root=~/SSLST/data-bin
