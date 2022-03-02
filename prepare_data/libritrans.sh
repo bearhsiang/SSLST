922094f3c273cb85badc2bc55d31b229487846cb
bearhsiang <b05902066@ntu.edu.tw>
2022-03-02 15:57:16 +0800

    adjust libritrans script

--- prepare_data/libritrans.sh
+++ prepare_data/libritrans.sh
@@ -1,10 +1,12 @@
 #!/usr/local/bin bash
 
+source script/setup.sh
+
 dataset=libritrans
-data_root=/hdd/libritrans
+data_root=$sslst_libritrans_root
 src_lang=en
 tgt_lang=fr
-output_root=data/tsv/
+output_root=$sslst_data_root/tsv
 
 python prepare_data/main.py \
     -d $dataset \
