a0ef6c968c4cb9431aa9a91efc2cff7c1ffa36b0
bearhsiang <b05902066@ntu.edu.tw>
2022-03-02 17:37:43 +0800

    add dump features

--- utils_new/dump_features.py
+++ utils_new/dump_features.py
@@ -30,9 +30,25 @@ def get_args():
     parser.add_argument('--rank', type=int)
     parser.add_argument('--device', default='cuda')
     parser.add_argument('--output-dir')
+    parser.add_argument('--format', choices=['seperate', 'collect'])
     args = parser.parse_args()
 
     return args
+
+def get_features(model, layer, device, audio_dir, audio_files):
+
+    for audio_file, n_frames in tqdm(audio_files):
+
+        wav, sr = sf.read(audio_dir/audio_file)
+        assert wav.shape[0] == int(n_frames)
+        assert sr == S3PRL_SR
+
+        with torch.no_grad():
+
+            feat = model([torch.tensor(wav, dtype=torch.float).to(device)])
+            feat = feat['hidden_states'][layer][0]
+
+        yield feat
 
 def main(args):
 
@@ -66,29 +82,24 @@ def main(args):
     output_dir = Path(args.output_dir)
     output_dir.mkdir(parents=True, exist_ok=True)
     
-    output_feat = output_dir/f'{args.split}_{args.rank}_{args.nshard}.npy'
-    output_len = output_dir/f'{args.split}_{args.rank}_{args.nshard}.len'
+    if args.format == 'collect':
 
-    feat_f = NpyAppendArray(output_feat)
-    len_f = open(output_len, 'w')
+        output_feat = output_dir/f'{args.split}_{args.rank}_{args.nshard}.npy'
+        output_len = output_dir/f'{args.split}_{args.rank}_{args.nshard}.len'
 
-    for audio_file, n_frames in tqdm(audio_files):
+        feat_f = NpyAppendArray(output_feat)
+        len_f = open(output_len, 'w')
 
-        wav, sr = sf.read(audio_dir/audio_file)
-        assert wav.shape[0] == int(n_frames)
-        assert sr == S3PRL_SR
+        for feat in get_features(model, args.layer, args.device, audio_dir, audio_files):
 
-        with torch.no_grad():
+            feat_f.append(feat.cpu().numpy())
+            print(feat.size(0), file=len_f)
+        
+        len_f.close()
 
-            feat = model([torch.tensor(wav, dtype=torch.float).to(args.device)])
-            feat = feat['hidden_states']
-            feat = feat[args.layer]
-            feat = feat[0]
+    elif args.format == 'seperate':
 
-        feat_f.append(feat.cpu().numpy())
-        print(feat.size(0), file=len_f)
-    
-    len_f.close()
+        raise NotImplementedError
 
 
 if __name__ == '__main__':
