263fc789194508b2544155eda120a53246147f4f
bearhsiang <b05902066@ntu.edu.tw>
2022-03-02 16:02:46 +0800

    add libritrans manifest

--- script/s2u/create_manifest_libritrans.sh
+++ script/s2u/create_manifest_libritrans.sh
@@ -1,16 +1,13 @@
 #!/usr/local/bin bash 
 
 dataset=libritrans-en-fr
-data_root=data
-tsv_root=data/tsv
 splits="train dev test"
 audio_key=audio
-libritrans_root=/hdd/libritrans
 
 for split in $splits; do
     python utils_new/create_fs_manifest.py \
-        --audio-root $libritrans_root \
-        --tsv-file $tsv_root/$dataset/$split.tsv \
+        --audio-root $sslst_libritrans_root \
+        --tsv-file $sslst_data_root/tsv/$dataset/$split.tsv \
         --key $audio_key \
-        --output $data_root/$dataset/manifest/$split.tsv
+        --output $sslst_data_root/$dataset/manifest/$split.tsv
 done
