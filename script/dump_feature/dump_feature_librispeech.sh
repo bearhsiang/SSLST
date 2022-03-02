c957026c437bb8724a5e7a3da0a5b553d22e54ca
bearhsiang <b05902066@ntu.edu.tw>
2022-03-02 15:39:54 +0800

    add dump ls

---
+++ script/dump_feature/dump_feature_librispeech.sh
@@ -0,0 +1,30 @@
+#!/usr/local/bin bash
+
+source script/setup.sh
+
+dataset=librispeech
+format=collect
+
+# model=modified_cpc
+model=$1
+# layer=1
+layer=$2
+nshard=5
+
+splits='train-clean-100'
+device='cuda'
+
+for split in $splits; do 
+    for rank in `seq 0 $((nshard -1))`; do
+        python utils_new/dump_features.py \
+            --manifest $sslst_data_root/$dataset/manifest \
+            --split $split \
+            --model $model \
+            --layer $layer \
+            --nshard $nshard \
+            --rank $rank \
+            --device $device \
+            --output-dir $sslst_feat_root/$dataset/$model/$layer \
+            --format $format
+    done
+done
