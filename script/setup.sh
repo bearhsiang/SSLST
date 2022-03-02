58e7cfa718d594d97682f51dff14404bdbd72565
bearhsiang <b05902066@ntu.edu.tw>
2022-03-02 15:48:38 +0800

    update setup.sh

--- script/setup.sh
+++ script/setup.sh
@@ -1,6 +1,10 @@
 #!/usr/local/bin bash
 
-export sslst_data_root=~/SSLST/data
-export sslst_feat_root=/ssd/ssl_feat
+export sslst_data_root=~/Desktop/SSLST/data
+export sslst_feat_root=/hdd/ssl_feat
 export sslst_output_root=/hdd/ssl_results
-export sslst_data_bin_root=~/SSLST/data-bin
+export sslst_data_bin_root=~/Desktop/SSLST/data-bin
+
+# dataset 
+export sslst_libritrans_root=/hdd/libritrans
+export sslst_librispeech_root=/hdd/LibriSpeech
