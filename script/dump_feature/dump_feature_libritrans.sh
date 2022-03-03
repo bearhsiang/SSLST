de8612b4f0cdc5e55d20c56f771328f7f01ce107
bearhsiang <b05902066@ntu.edu.tw>
2022-03-03 15:03:50 +0800

    skip when file exists

--- script/dump_feature/dump_feature_libritrans.sh
+++ script/dump_feature/dump_feature_libritrans.sh
@@ -16,15 +16,19 @@ device='cuda'
 
 for split in $splits; do 
     for rank in `seq 0 $((nshard -1))`; do
-        python utils_new/dump_features.py \
-            --manifest $sslst_data_root/$dataset/manifest \
-            --split $split \
-            --model $model \
-            --layer $layer \
-            --nshard $nshard \
-            --rank $rank \
-            --device $device \
-            --output-dir $sslst_feat_root/$dataset/$model/$layer \
-            --format $format
+        if [ ! -f $sslst_feat_root/$dataset/$model/$layer/${split}_${rank}_${nshard}.npy ] && [ $format == "collect" ]; then
+            python utils_new/dump_features.py \
+                --manifest $sslst_data_root/$dataset/manifest \
+                --split $split \
+                --model $model \
+                --layer $layer \
+                --nshard $nshard \
+                --rank $rank \
+                --device $device \
+                --output-dir $sslst_feat_root/$dataset/$model/$layer \
+                --format $format
+        else
+            echo $sslst_feat_root/$dataset/$model/$layer/${split}_${rank}_${nshard}.npy exists, skip...
+        fi
     done
 done
