cd561b931cff2d3829b3ae7f7c344217e827abdb
bearhsiang <b05902066@ntu.edu.tw>
2022-03-04 08:43:48 +0800

    add generate s2t

--- script/generate/generate_s2t.sh
+++ script/generate/generate_s2t.sh
@@ -2,7 +2,6 @@
 
 source script/setup.sh
 
-data_root=data
 name=libritrans-en-fr
 
 arch=s2t_transformer_s
@@ -10,7 +9,7 @@ src_lang=hubert_l9
 tgt_lang=fr
 
 fairseq-generate \
-    $data_root/$name/s2t-$src_lang-$tgt_lang \
+    $sslst_data_root/$name/s2t-$src_lang-$tgt_lang \
     --config-yaml config.yaml \
     --gen-subset test --task speech_to_text \
     --path $sslst_output_root/$name/$arch-$src_lang-$tgt_lang/checkpoint_best.pt \
