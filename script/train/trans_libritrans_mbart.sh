8602221c4b24377805618f98ec1ad682a707a046
bearhsiang <b05902066@ntu.edu.tw>
2022-03-14 15:58:05 +0800

    add mbart

--- script/train/trans_libritrans_mbart.sh
+++ script/train/trans_libritrans_mbart.sh
@@ -2,9 +2,10 @@
 
 source script/setup.sh
 
-name=libritrans-en-fr
-src_lang=en_XX
-tgt_lang=fr_XX
+name=$1
+src_lang=$2
+tgt_lang=$3
+
 langs=ar_AR,cs_CZ,de_DE,en_XX,es_XX,et_EE,fi_FI,fr_XX,gu_IN,hi_IN,it_IT,ja_XX,kk_KZ,ko_KR,lt_LT,lv_LV,my_MM,ne_NP,nl_XX,ro_RO,ru_RU,si_LK,tr_TR,vi_VN,zh_CN
 pretrained_model=$sslst_data_root/mbart.cc25.v2/model.pt
 
@@ -23,11 +24,14 @@ fairseq-train \
     --dropout 0.3 --attention-dropout 0.1 --weight-decay 0.0 \
     --max-tokens 1024 --update-freq 2 \
     --save-interval 1 --save-interval-updates 5000 --keep-interval-updates 10 --no-epoch-checkpoints \
-    --seed 222 --log-format simple --log-interval 2 \
+    --seed $sslst_seed --log-format simple --log-interval 2 \
     --restore-file $pretrained_model \
     --reset-optimizer --reset-meters --reset-dataloader --reset-lr-scheduler \
     --langs $langs \
-    --ddp-backend legacy_ddp
+    --ddp-backend legacy_ddp \
+    --keep-last-epochs 3 \
+    --fp16
+
 
 # fairseq-train \
 #     $data_dir \
