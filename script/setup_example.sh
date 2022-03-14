20c50e435928b2037763ca95933e6b3f142a5765
bearhsiang <b05902066@ntu.edu.tw>
2022-03-14 15:59:18 +0800

    add setup example

---
+++ script/setup_example.sh
@@ -0,0 +1,16 @@
+#!/usr/local/bin bash
+
+export sslst_seed=1
+
+export sslst_data_root=/hdd/sslst_data
+export sslst_feat_root=/ssd/sslst_feat
+export sslst_output_root=/hdd/sslst_results
+export sslst_data_bin_root=/hdd/sslst_databin
+
+# dataset 
+export sslst_libritrans_root=/hdd/libritrans
+export sslst_librispeech_root=/hdd/LibriSpeech
+
+# repo
+export sslst_mosesdecoder_root=~/Desktop/SSLST/mosesdecoder
+export sslst_fairseq_root=~/Desktop/SSLST/fairseq
